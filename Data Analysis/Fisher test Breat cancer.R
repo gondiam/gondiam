7494e4ba61f6645dd351edb5b7e972a8d0c6e334
gondiam <80326419+gondiam@users.noreply.github.com>
2021-11-15 15:53:53 +0100

    Delete Data Analysis directory

--- Data Analysis/Fisher test Breat cancer.R
+++
@@ -1,6 +0,0 @@
-#Fisher test
-TeaTasting<-matrix(c(39, 30961, 63, 30937),
-       nrow = 2,
-       dimnames = list(Guess = c("Death", "Survive"),
-                       Group = c("Treatment", "Control")));TeaTasting
-fisher.test(TeaTasting, alternative = "less")
